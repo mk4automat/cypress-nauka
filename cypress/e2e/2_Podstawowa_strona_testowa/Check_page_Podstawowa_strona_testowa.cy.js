682ce83bef61af4d131c7c977c8021672b9901a4
mk tester automatyczny <mk4testy@gmail.com>
2022-11-18 14:02:34 +0100

    commit 3

--- cypress/e2e/2_Podstawowa_strona_testowa/Check_page_Podstawowa_strona_testowa.cy.js
+++ cypress/e2e/2_Podstawowa_strona_testowa/Check_page_Podstawowa_strona_testowa.cy.js
@@ -11,7 +11,7 @@ describe('Check page Podstawowa strona testowa', () => {
     it('Should check page', () => {
 
         // //Nagłówek - dodanie polecenia i sprawdzenie tła po najechaniu 
-        // cy.get('h1')
+        // cy.get('h1') 
         //     .realHover()
         //     .should('have.css', 'background-color', 'rgb(255, 255, 0)')
 
